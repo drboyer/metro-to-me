c59c0ffebda01d81f1dd79ee23931620053fca36
Devin Boyer <drb5272@gmail.com>
2021-07-06 23:24:33 -0400

    fix prod path

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,4 @@
 module.exports = {
   publicPath:
-    process.env.NODE_ENV === "production" ? "/mapbox-vue-example/" : "/",
+    process.env.NODE_ENV === "production" ? "/" : "/",  // on DigitalOcean it's the same either way
 };
