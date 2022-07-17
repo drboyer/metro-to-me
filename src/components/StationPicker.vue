a3118d4a520173fd8a484795e063fffeaf10b5a3
Devin Boyer <drb5272@gmail.com>
2022-07-17 14:07:40 -0400

    add a searchable list

--- src/components/StationPicker.vue
+++ src/components/StationPicker.vue
@@ -1,21 +1,21 @@
 <template>
   <div id="selector-ui">
     Walking distances from
-    <select
+    <vSelect
+      class="selector-dropdown"
       v-if="stations"
+      :options="stations"
       v-model="selectedStationName"
-      @change="emitPickerEvent($event)"
-    >
-      <option v-for="station in stations" v-bind:key="station">
-        {{ station }}
-      </option>
-    </select>
+      @input="emitPickerEvent"
+      placeholder="Hello">
+    </vSelect>
     Station:
   </div>
 </template>
 
 <script>
 import { eventBus, stationData } from "../main";
+import 'vue-select/dist/vue-select.css';
 
 export default {
   name: "StationPicker",
@@ -35,11 +35,20 @@ export default {
       eventBus.$emit("station-picker-changed", this.selectedStationName);
     },
   },
+  components: {
+    vSelect: () => import('vue-select')
+  }
 };
 </script>
 
 <style scoped>
 #selector-ui {
   padding-bottom: 1em;
+}
+
+.selector-dropdown {
+  width: 24em;
+  display: inline-block;
+  vertical-align: middle;
 }
 </style>
